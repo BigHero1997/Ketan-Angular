8f00f6d0603327ee24b315085d92a221e274fd25
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-11 15:48:37 +0530

    Start new cooment

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular-tutorial');
+  name = 'Angular';
+  userName = 'Ketan';
+  userId = 12345;
+  isMarried = true;
+  isAlive = true;
+}
