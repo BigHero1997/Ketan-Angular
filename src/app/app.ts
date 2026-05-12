c6bcde9396812a6a87af511a272961759b6f0b1b
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-12 14:01:14 +0530

    work done for cust comp on 12-05-2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { signInComponent } from './signin/signin';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login,signInComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/signin/signin.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-signin',
+    templateUrl: './signin.html',
+    styleUrl: './signin.css',
+    // template: `
+    //          <p>sign-in works!</p>
+    //         `,
+    // styles: [`
+    //  p {
+    //         color: red;
+    //     }
+    //     `]
+
+})
+
+export class signInComponent {
+    signInTitle = 'Sign In Page';
+}
