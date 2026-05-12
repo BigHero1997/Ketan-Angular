fb71d8627ef95eb081bbb3b65c01b2f9f4a4b3e7
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-12 18:39:12 +0530

    counter app created

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,10 +8,49 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-tutorial');
-  name = 'Angular';
-  userName = 'Ketan';
-  userId = 12345;
-  isMarried = true;
-  isAlive = true;
+  // protected readonly title = signal('angular-tutorial');
+  // name = 'Angular';
+  // userName = 'Ketan';
+  // userId = 12345;
+  // isMarried = true;
+  // isAlive = true;
+
+
+
+
+
+counter: number = 0;
+
+
+// handleAdd() {
+//   this.counter++;
+
+// }
+
+// handleSubtract() {
+//   if(this.counter > 0) {
+//     this.counter--;
+//   }
+// }
+
+// handleReset() {
+//   this.counter = 0;   
+// }
+
+
+handle(value: string) {
+  if(value === 'plus') {
+    this.counter++;
+  }
+  else if(value === 'minus') {
+    if(this.counter > 0) {
+      this.counter--;
+    }
+  }
+  else{
+    this.counter = 0;
+  }
+
+}
+
 }
