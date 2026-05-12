e9c6f4d5e1cab21a32ad87b43113ec6b045839b2
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-12 20:21:58 +0530

    done changes for get set fields

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,10 +8,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-tutorial');
-  name = 'Angular';
-  userName = 'Ketan';
-  userId = 12345;
-  isMarried = true;
-  isAlive = true;
+//   protected readonly title = signal('angular-tutorial');
+//   name = 'Angular';
+//   userName = 'Ketan';
+//   userId = 12345;
+//   isMarried = true;
+//   isAlive = true;
+// 
+
+name: string = "";
+city: string = "";
+email: string = "";
+
+
+updateName(value: string) {
+  this.name = value;
+}
+
+getEmail(email: string) {
+  this.email = email;
+}
+
 }
