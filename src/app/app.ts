d31fbf7146864d006e597e944d8f755ded921ef2
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-12 18:08:01 +0530

    work done for events

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,10 +8,42 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-tutorial');
-  name = 'Angular';
-  userName = 'Ketan';
-  userId = 12345;
-  isMarried = true;
-  isAlive = true;
+  // protected readonly title = signal('angular-tutorial');
+  // name = 'Angular';
+  // userName = 'Ketan';
+  // userId = 12345;
+  // isMarried = true;
+  // isAlive = true;
+
+// for click event
+  handleClick() {
+    console.log('Button clicked!');
+  }
+//for input event
+  handleInput(event: any) {
+    console.log('Input typed:', event.target.value);
+  }
+//for key up event and key down event
+  handleKeyUp(event: any) {
+    console.log('Key up event:', event.key);
+  }
+
+  //mouse hover event
+  handleMouseHover() {
+    console.log('Mouse hovered over the element!');
+  }
+ //mouse leave event
+  handleMouseLeave() {
+    console.log('Mouse left the element!');
+  }
+
+  //blur event
+  handleBlur() {
+    console.log('Input field lost focus!');
+  }
+
+  //focus event
+  handleFocus() {
+    console.log('Input field gained focus!');
+  }
 }
