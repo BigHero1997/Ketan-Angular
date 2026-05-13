f14618cf9937551d6a881fcb3316454e11769ddc
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-13 14:42:41 +0530

    angular styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,14 +4,26 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
+  template: `<h2 class="title">Welcome to My App</h2>`,
+  styles: `
+    .title {
+      border-radius: 10px;
+    padding: 20px;
+    background-color: #ca9f9f;
+    border: 1px solid #ccc;
+    padding: 20px;
+    text-align: center;
+    font-size: 24px;
+    color: #cb1111;
+    margin-top: 20px;
+    }
+  `})
+
 export class App {
-  protected readonly title = signal('angular-tutorial');
-  name = 'Angular';
-  userName = 'Ketan';
-  userId = 12345;
-  isMarried = true;
-  isAlive = true;
+  // protected readonly title = signal('angular-tutorial');
+  // name = 'Angular';
+  // userName = 'Ketan';
+  // userId = 12345;
+  // isMarried = true;
+  // isAlive = true;
 }
