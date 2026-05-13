d311d32e414bc435a4aaa386d2169cb29cb6d7e3
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-13 18:09:52 +0530

    for loops, ,first,last,odd,even

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,10 +8,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-tutorial');
-  name = 'Angular';
-  userName = 'Ketan';
-  userId = 12345;
-  isMarried = true;
-  isAlive = true;
+  // protected readonly title = signal('angular-tutorial');
+  // name = 'Angular';
+  // userName = 'Ketan';
+  // userId = 12345;
+  // isMarried = true;
+  // isAlive = true;
+
+
+
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  //  items = ['html', 'css', 'javascript', 'angular'];
+
+  // users = [
+  //   { name: 'Ketan', age: 30 },
+  //   { name: 'John', age: 25 },
+  //   { name: 'Alice', age: 28 },
+  //   { name: 'Bob', age: 35 }
+  // ];
+
+  // items = ['apple', 'banana', 'cherry', 'date'];
+
+  // deleteItem(i: number) {
+  //   this.items.splice(i, 1);
+  // } 
 }
