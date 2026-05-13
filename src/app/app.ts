5a6937e316ca16a89b81ca8a845533432f09574b
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-13 15:57:44 +0530

    if-elseif

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-tutorial');
-  name = 'Angular';
-  userName = 'Ketan';
-  userId = 12345;
-  isMarried = true;
-  isAlive = true;
+  // protected readonly title = signal('angular-tutorial');
+  // name = 'Angular';
+  // userName = 'Ketan';
+  // userId = 12345;
+  // isMarried = true;
+  isAlive = false;
+  marks = 45;
+  showBox = true;
+  age = 0;
+  section = "home";
+  updateAge(value: string) {
+    this.age = Number(value);
+
+  }
+
+  toggleBox() {
+    //if false then true and if true then false
+    this.showBox = !this.showBox;
+  }
 }
