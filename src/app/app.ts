2e9c1754b475492ef6b6299dcd1915bc7df616db
Ketan Kulawade <ketankulawade@gmail.com>
2026-05-14 13:01:02 +0530

    events

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,58 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+
+  // count = signal(0);
+
+  // constructor() {
+  //   effect(() => {
+  //     console.log("Count is ", this.count());
+  //   });
+
+  // }
+
+
+  // increment () {
+  //   this.count.update(count => count + 1);
+  // }
  
+
+  isDarkMode = signal(false);
+
+  // constructor() {
+  //   effect(() => {
+  //     if (this.isDarkMode()) {
+  //       document.body.style.backgroundColor = 'black';
+  //       document.body.style.color = 'white';
+        
+  //     } else {
+  //       document.body.style.backgroundColor = 'white';
+  //       document.body.style.color = 'black';
+  //     }
+  //   });
+  // }
+
+  // toggleDarkMode() {
+  //   this.isDarkMode.update(isDark => !isDark);
+  // }
+
+
+message = signal("");
+
+  constructor() {
+    effect(() => {
+      if (this.message()) {
+        setTimeout(() => {
+          this.message.set("");
+        }, 5000);
+      } 
+  });
+}
+  
+
+showMessage() {
+  this.message.set("Hello World");
+
+}
+
 }
